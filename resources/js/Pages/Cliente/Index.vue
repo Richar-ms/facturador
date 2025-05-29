22a4c3d334c5f1f85db112435594162be2b5b854
fcva-senati <fvigilioa@senati.pe>
2025-05-29 11:11:51 -0500

    clientes

---
+++ resources/js/Pages/Cliente/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link, usePage } from '@inertiajs/vue3';
+
+
+const page = usePage()
+
+console.log(page.props.clientes);
+
+
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <ul v-for="cliente in page.props.clientes">
+                    <li>{{ cliente.nombres }}</li>
+                    <li>{{ cliente.pri_ape }}</li>
+                    <li>{{ cliente.seg_ape }}</li>
+                </ul>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
